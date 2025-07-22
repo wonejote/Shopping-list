cce1ed03ff5d931fd76616ca86e8a703d0403bb9
Antonio <antonio.pulgarin97@gmail.com>
2025-07-22 16:01:36 -0500

    se agregó botón para añadir cartas con nombre y numero

--- script.js
+++ script.js
@@ -9,6 +9,35 @@ function addBookToLibrary(name, amount) {
   let newBoowk = new Book(name,amount);
   myLibrary.push(newBoowk);
 }
+//-------------------------------------------------
+const bodyElement = document.querySelector("body");
+const botonElement = document.querySelector("#agregar");
+const inputName = document.querySelector("#nameid");
+const inputNumber = document.querySelector("#numberid");
+
+
+botonElement.addEventListener("click",function()
+
+{
+
+  let card = document.createElement("div");
+  card.classList.add("card");
+  card.id = crypto.randomUUID();
+  card.innerText = inputName.value;
+  inputName.value = "";
+
+
+
+bodyElement.appendChild(card);
+
+
+
+});
+
+
+
+
+
 
 console.log(myLibrary);
 addBookToLibrary("uno",2);
